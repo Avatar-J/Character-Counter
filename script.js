73ca4702651fd56fbd1a0bc4ae23d6c3459226f9
Jummai <jummyhassan5@gmail.com>
2025-04-17 05:36:17 +0000

    added functions to count the letters in text
    the functions counts the occurrence of each letter and calculates the percentage

--- script.js
+++ script.js
@@ -17,62 +17,188 @@ window.addEventListener("DOMContentLoaded", function () {
     isDarkActive = !isDarkActive;
   });
 
-  //text area and letter density section
+  //text area section
   const textArea = document.getElementById("text-area");
   let textValue = textArea.value;
   const charCountContainer = document.getElementById("char-count");
   const wordCountContainer = document.getElementById("word-count");
   const sentenceCountContainer = document.getElementById("sentence-count");
-
-  let letterDensitySection = document.querySelector(".not-empty-textarea");
-  let emptyText = document.querySelector(".empty-textarea");
+  const excludeSpacesToggle = document.getElementById("spaces");
+  const setCharacterLimitToggle = document.getElementById("limit");
+  const characterLimitContainer = document.getElementById("limit-box");
+  const errorMessageContainer = document.getElementById("error-message").style;
+  const limitValueContainer = document.getElementById("limit-value");
+  let limit = null;
+  let isExcludeSpaceChecked = false;
+  let isSetCharacterLimitChecked = false;
+  let charCount = null;
 
   //when you first load page and text area is empty
-  letterDensitySection.classList.add("hidden");
+  const letterDensitySection = document.querySelector(".not-empty-textarea");
+  //letterDensitySection.classList.add("hidden");
 
+  //function to pad a number with zero if the digits are less than 2
+  function padZero(number) {
+    return number.toString().padStart(2, "0");
+  }
+
+  function errorStyleRemove() {
+    errorMessageContainer.display = "none";
+    textArea.classList.remove("error-state");
+    textArea.classList.add("text-area");
+  }
+
+  //function to display error message when limit is exceeded
+  function displayErrorMessage(count) {
+    if (isSetCharacterLimitChecked) {
+      if (limit != null) {
+        if (count > limit) {
+          limitValueContainer.textContent = limit;
+          errorMessageContainer.display = "block";
+          textArea.classList.add("error-state");
+          textArea.classList.remove("text-area");
+        } else {
+          errorStyleRemove();
+        }
+      }
+    }
+  }
+  //function to exclude and include spaces in text
+  function excludeSpaces() {
+    charCount = textValue.replace(/\s+/g, "").length;
+    charCountContainer.textContent = padZero(charCount);
+  }
+
+  function includeSpaces() {
+    charCount = textValue.length;
+    charCountContainer.textContent = padZero(charCount);
+  }
+
+  function countLetters() {
+    if (textValue.trim() != "") {
+      const converttoUpperCase = textValue.toUpperCase();
+      const letterFrequency = {};
+      const letterPercentages = {};
+      // const letterDensityHTML = {};
+      let totalCharacters = 0;
+
+      for (let i = 0; i < converttoUpperCase.length; i++) {
+        const letter = converttoUpperCase[i];
+        if (letter >= "A" && letter <= "Z") {
+          totalCharacters++;
+          if (letterFrequency[letter]) {
+            letterFrequency[letter]++;
+          } else {
+            letterFrequency[letter] = 1;
+          }
+        }
+      }
+      for (const letter in letterFrequency) {
+        letterPercentages[letter] = (
+          (letterFrequency[letter] / totalCharacters) *
+          100
+        ).toFixed(2);
+      }
+
+      letterDensitySection.innerHTML = ""; // Clear previous content
+      let letterDensityHTML = "";
+      for (const letter in letterFrequency) {
+        renderLetterDensity(
+          letter,
+          letterPercentages,
+          letterFrequency,
+          letterDensityHTML
+        );
+      }
+      letterDensitySection.innerHTML = letterDensityHTML;
+      console.log("letter freq", letterFrequency);
+      console.log("percentages", letterPercentages);
+    }
+  }
+  //function to render letter density percentage
+  function renderLetterDensity(aletter, aletterPercentages, aletterFrequency) {
+    letterDensityHTML += `
+    <div class="ld-letter">
+      <span class="letter">${aletter}</span>
+      <div class="ld-percentage">
+        <div class="percentage" style="width: ${aletterPercentages[aletter]}"></div>
+      </div>
+      <span class="value">${aletterFrequency[aletter]}(${aletterPercentages[aletter]})</span>
+    </div>
+   `;
+  }
+
+  //event handlers
+  excludeSpacesToggle.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      excludeSpaces();
+      isExcludeSpaceChecked = true;
+    } else {
+      includeSpaces();
+      isExcludeSpaceChecked = false;
+    }
+    displayErrorMessage(charCount);
+  });
+
+  setCharacterLimitToggle.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      this.document.getElementById("limit-box").style.display = "block";
+      isSetCharacterLimitChecked = true;
+    } else {
+      this.document.getElementById("limit-box").style.display = "none";
+      isSetCharacterLimitChecked = false;
+      limit = null;
+      errorStyleRemove();
+    }
+  });
+
+  characterLimitContainer.addEventListener("input", (event) => {
+    limit = event.target.value;
+    displayErrorMessage(charCount);
+  });
+
+  //listen for input in text area
   textArea.addEventListener("input", (event) => {
     textValue = event.target.value;
 
-    const charCount = textValue.length;
+    if (isExcludeSpaceChecked) {
+      excludeSpaces();
+    } else {
+      includeSpaces();
+    }
+
+    displayErrorMessage(charCount);
+
     const wordCount = textValue
       .split(/\s+/)
-      .filter((word) => word.length > 0).length;
+      .filter((word) => word.trim().length > 0).length;
     const sentenceCount = textValue
       .split(/[.!?]+/)
       .filter((sentence) => sentence.trim().length > 0).length;
 
-    charCountContainer.textContent = charCount;
-    wordCountContainer.textContent = wordCount;
-    sentenceCountContainer.textContent = sentenceCount;
+    wordCountContainer.textContent = padZero(wordCount);
+    sentenceCountContainer.textContent = padZero(sentenceCount);
 
-    //make letter density section visible if text area is not empty
-    if (textValue.trim() === "") {
-      if (emptyText.classList.contains("hidden")) {
-        emptyText.classList.remove("hidden");
-      }
-      if (!letterDensitySection.classList.contains("hidden")) {
-        letterDensitySection.classList.add("hidden");
-      }
-    }
+    countLetters();
   });
 
-  //   //when you start typing in text area
+  //letter density section
 
-  //   textArea.addEventListener("input", function (event) {
-  //     textValue = event.target.value;
-  //     console.log(textValue);
+  const emptyText = document.querySelector(".empty-textarea");
+  let isEmptyTextHidden = emptyText.classList.contains("hidden");
+  let isLetterDensitySectionHidden =
+    letterDensitySection.classList.contains("hidden");
 
-  //     //make letter density section visible if text area is not empty
-  //     if (textValue.trim() === "") {
-  //       if (emptyText.classList.contains("hidden")) {
-  //         emptyText.classList.remove("hidden");
-  //       }
-  //       if (!letterDensitySection.classList.contains("hidden")) {
-  //         letterDensitySection.classList.add("hidden");
-  //       }
-  //     } else {
-  //       emptyText.classList.add("hidden");
-  //       letterDensitySection.classList.remove("hidden");
+  //display letter density percentage
+
+  // if (charCount != null) {
+  //   if (charCount.trim() === "") {
+  //     if (isEmptyTextHidden) {
+  //       emptyText.classList.remove("hidden");
   //     }
-  //   });
+  //     if (!isLetterDensitySectionHidden) {
+  //       letterDensitySection.classList.add("hidden");
+  //     }
+  //   }
+  // }
 });
